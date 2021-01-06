cd24e43100c2c757d222f1b35ca644e364be01bd
AIA-UnKNOWN <AIA-UnKNOWN>
2021-01-06 15:49:59 +0800

    Changed the icons resources

--- passwordmanager/tests.py
+++ passwordmanager/tests.py
@@ -4,48 +4,23 @@ from django.urls import reverse
 from .models import Account
 # Create your tests here.
 
-class AccountIndexViewTests(TestCase):
+
+class LoginViewTests(TestCase):
 	
-	def test_no_accounts(self):
-		url = reverse('pm:index')
-		response = self.client.get(url)
-		
-		self.assertContains(response, "No accounts are available.")
-		self.assertQuerysetEqual(
-			response.context['accounts'],
-			[]
-		)
-		
-	def test_account(self):
-		Account.objects.create(
-			title="My Facebook",
-			username="myfbuser",
-			password="12345"
-		)
-		url = reverse('pm:index')
-		response = self.client.get(url)
-		
-		self.assertQuerysetEqual(
-			response.context['accounts'],
-			['<Account: My Facebook>']
-		)
-		
-	def test_accounts(self):
-		Account.objects.create(
-			title="Youtube", 
-			username="cathcath101",
-			password="xxx0000xxx"
-		)
-		Account.objects.create(
-			title="Facebook",
-			username="catherine101",
-			password="100100100"
-		)
-		
-		url = reverse('pm:index')
-		response = self.client.get(url)
-		
-		self.assertQuerysetEqual(
-			response.context['accounts'],
-			['<Account: Youtube>', '<Account: Facebook>'],
-		)
+	# Test for validating users
+	
+	pass
+	
+	
+class RegisterViewTests(TestCase):
+	
+	# Test for existing user
+	
+	pass
+	
+	
+class IndexViewTests(TestCase):
+	
+	# Test for index view
+	
+	pass
